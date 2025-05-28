67539277877344315dd1342a63a1cda57c0ea310
HitoshiMiguel <miguelhitoshi@gmail.com>
2025-05-27 22:17:25 -0300

    Atividade dia 22-05 e organização dos arquivos por pasta

---
+++ Atividade-22-05/leitorEspacos.py
@@ -0,0 +1,3 @@
+A, B, C, D = map(float, input("Digite os números desejados: ").split(" "))
+
+print("A com", A, "B com", B, "C com", C, "D com", D)
---
+++ Atividade-22-05/leitorString.py
@@ -0,0 +1,7 @@
+A = input("Digite um número inteiro: ")
+B = int(A)
+
+if B < 0:
+    print("Erro, por favor digite novamente")
+else:
+    print(B)
